534d31bbdfa888a9ff2f9a5c733ff25e63a48114
unknown <dale.hurwitz@SYD-DIG-DT17.macsc.net.au>
2015-07-15 17:39:37 +1000

    added google autocomplete, changed twitter module

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ var http_serv = http.createServer(handleHTTP).listen(port, host);
 
 var fs = require("fs");
 var io = require("socket.io").listen(http_serv);
-var twitter = require("ntwitter");
+var twitter = require("twitter");
+
+var GoogleLocations = require('google-locations');
+var locations = new GoogleLocations('AIzaSyBlGLYy8NRJcU_oPUseU6PDy4GT3S_2bCI');
+
+locations.autocomplete({input: 'Verm'}, function(err, response) {
+	console.log("autocomplete: ", response.predictions);
+});
 
 io.on("connection", handleIO);
 io.configure(function(){
@@ -14,34 +21,38 @@ io.configure(function(){
 });
 
 var twit = new twitter({
-    consumer_key: 'SKKs1PSexqKHddYG5VAoz1Ekk',
-    consumer_secret: 'VtPwqBZ2LFRH0yMxDIy6RzN5bAwFmSf3lsDeM95xO1nb0Rru53',
-    access_token_key: '121646999-5XGNK3X5cwqiLocBEvCwKIkkvGtxRADnmWNPxTbX',
-    access_token_secret: '9NVtIIu4yPbLek7YQkRhOTLoP69rEvGJ7uA9PqKZdIPNp'
+	consumer_key: 'SKKs1PSexqKHddYG5VAoz1Ekk',
+	consumer_secret: 'VtPwqBZ2LFRH0yMxDIy6RzN5bAwFmSf3lsDeM95xO1nb0Rru53',
+	access_token_key: '121646999-5XGNK3X5cwqiLocBEvCwKIkkvGtxRADnmWNPxTbX',
+	access_token_secret: '9NVtIIu4yPbLek7YQkRhOTLoP69rEvGJ7uA9PqKZdIPNp'
 });
 
-twit.stream('statuses/filter', {track:'ufc'}, function(stream) {
-    stream.on('data', function (tweet) {
-        //console.log(tweet);
-    });
+var num = 0;
+twit.stream('statuses/filter', {track:'GrowingUpMexican'}, function(stream) {
+	stream.on('data', function (tweet) {
+		if(typeof tweet.user !== "undefined" && tweet.user.location && num < 5) {
+			num ++;
+			console.log(tweet.user.location);
+		}
+	});
 });
 
 function handleHTTP(req, res) {
-    fs.readFile(__dirname + "/index.html",
-        function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end("Error loading index.html");
-            }
-            res.writeHead(200);
-            res.end(data);
-        });
+	fs.readFile(__dirname + "/index.html",
+				function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end("Error loading index.html");
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
 }
 
 function handleIO(socket) {
-    console.log("connected");
-    
-    socket.on("disconnect", function() {
-        console.log("disconnected");
-    });
+	console.log("connected");
+
+	socket.on("disconnect", function() {
+		console.log("disconnected");
+	});
 }
